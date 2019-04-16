6c16e241d23fb02ed2287adc6cc4226420413a9b
JonathanOnorato <46499087+JonathanOnorato@users.noreply.github.com>
2019-04-15 22:09:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
 
+setup(name='SolarAlaska',
+      version='0.1.beta',
+      description='A python package for analyzing and displaying solar panel data.',
+      url='https://github.com/acep-solar/ACEP_solar',
+      author='ACEP Solar Development Team, University of Washington (2019)',
+      license='MIT',
+      packages=['SolarAlaska'],
+      install_requires=['numpy', 'Folium', 'matplotlib', 'pandas'])
