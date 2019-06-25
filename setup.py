70fd7cba20aa2802a4e6162e349d80739df161ef
Jon Onorato <jononorato@gmail.com>
2019-06-25 12:10:37 -0700

    Fixed issue with requesting JSON install

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='SolarAlaska',
+setup(name='ARCTIC',
       version='0.1.beta',
       description='A python package for analyzing and displaying solar panel data.',
       url='https://github.com/acep-solar/ACEP_solar',
-      author='ACEP Solar Development Team, University of Washington (2019)',
+      author='ARCTIC Development Team, University of Washington (2019)',
       license='MIT',
-      packages=['SolarAlaska'],
-      install_requires=['numpy', 'Folium', 'matplotlib', 'pandas'])
+      packages=['ARCTIC'],
+      install_requires=['numpy', 'Folium', 'bokeh', 'pandas', 'scikit-learn', 'h5py'])
